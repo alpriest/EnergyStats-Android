41c5c8f81e4eb61ef08d103320a56853acc3c6e7
Al Priest <alpriest@gmail.com>
2023-06-21 17:14:21 +0100

    Added highlight rectangle and date time of the selected graph point

--- app/src/main/java/com/alpriest/energystats/ui/paramsgraph/ParameterGraphVariableTogglesView.kt
+++ app/src/main/java/com/alpriest/energystats/ui/paramsgraph/ParameterGraphVariableTogglesView.kt
@@ -2,11 +2,16 @@ package com.alpriest.energystats.ui.paramsgraph
 
 import android.content.res.Resources
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alpriest.energystats.R
 import com.alpriest.energystats.preview.FakeConfigManager
 import com.alpriest.energystats.services.DemoNetworking
@@ -14,14 +19,18 @@ import com.alpriest.energystats.ui.flow.home.preview
 import com.alpriest.energystats.ui.ToggleRowView
 import com.alpriest.energystats.ui.theme.AppTheme
 import kotlinx.coroutines.flow.MutableStateFlow
+import java.text.DateFormat
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 @Composable
 fun ParameterGraphVariableTogglesView(viewModel: ParametersGraphTabViewModel, themeStream: MutableStateFlow<AppTheme>, modifier: Modifier = Modifier) {
     val graphVariables = viewModel.graphVariablesStream.collectAsState()
     val selectedValues = viewModel.valuesAtTimeStream.collectAsState().value
+    val selectedDateTime = selectedValues.firstOrNull()?.localDateTime
 
     Column(modifier) {
-        graphVariables.value.filter {it.isSelected}.map {
+        graphVariables.value.filter { it.isSelected }.map {
             val title = when (it.type.variable) {
                 "generationPower" -> "Output energy"
                 "feedinPower" -> "Feed-in energy"
@@ -45,6 +54,17 @@ fun ParameterGraphVariableTogglesView(viewModel: ParametersGraphTabViewModel, th
             val value = selectedValues.firstOrNull { entry -> entry.type == it.type }
 
             ToggleRowView(it, themeStream, { viewModel.toggleVisibility(it) }, title, description, value?.y?.toDouble())
+        }
+    }
+
+    selectedDateTime?.let {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 24.dp).fillMaxWidth()
+        ) {
+            Text(
+                text = it.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
+            )
         }
     }
 }
--- app/src/main/java/com/alpriest/energystats/ui/paramsgraph/ParameterGraphView.kt
+++ app/src/main/java/com/alpriest/energystats/ui/paramsgraph/ParameterGraphView.kt
@@ -4,9 +4,12 @@ import android.graphics.RectF
 import androidx.compose.animation.core.SnapSpec
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.alpriest.energystats.ui.statsgraph.chartStyle
 import com.patrykandpatrick.vico.compose.axis.axisLabelComponent
@@ -15,6 +18,7 @@ import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
 import com.patrykandpatrick.vico.compose.chart.line.lineChart
 import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollSpec
+import com.patrykandpatrick.vico.compose.component.lineComponent
 import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
 import com.patrykandpatrick.vico.core.axis.AxisPosition
 import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
@@ -22,6 +26,8 @@ import com.patrykandpatrick.vico.core.axis.formatter.DecimalFormatAxisValueForma
 import com.patrykandpatrick.vico.core.axis.horizontal.HorizontalAxis
 import com.patrykandpatrick.vico.core.chart.values.AxisValuesOverrider
 import com.patrykandpatrick.vico.core.chart.values.ChartValues
+import com.patrykandpatrick.vico.core.component.shape.DashedShape
+import com.patrykandpatrick.vico.core.component.shape.LineComponent
 import com.patrykandpatrick.vico.core.context.DrawContext
 import com.patrykandpatrick.vico.core.marker.Marker
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -48,7 +54,10 @@ fun ParameterGraphView(viewModel: ParametersGraphTabViewModel, modifier: Modifie
                     tickPosition = HorizontalAxis.TickPosition.Center(offset = 0, spacing = 20),
                     valueFormatter = ParameterGraphFormatAxisValueFormatter()
                 ),
-                marker = NonDisplayingMarker(viewModel.valuesAtTimeStream),
+                marker = NonDisplayingMarker(viewModel.valuesAtTimeStream, lineComponent(
+                    color = MaterialTheme.colorScheme.onSurface,
+                    thickness = 2.dp
+                )),
                 diffAnimationSpec = SnapSpec()
             )
         }
@@ -68,8 +77,31 @@ class ParameterGraphFormatAxisValueFormatter<Position : AxisPosition>() :
     }
 }
 
-class NonDisplayingMarker(var valuesAtTimeStream: MutableStateFlow<List<DateTimeFloatEntry>> = MutableStateFlow(listOf())) : Marker {
+class NonDisplayingMarker(
+    var valuesAtTimeStream: MutableStateFlow<List<DateTimeFloatEntry>> = MutableStateFlow(listOf()),
+    val guideline: LineComponent?
+) : Marker {
     override fun draw(context: DrawContext, bounds: RectF, markedEntries: List<Marker.EntryModel>) {
+        drawGuideline(context, bounds, markedEntries)
+
         valuesAtTimeStream.value = markedEntries.mapNotNull { it.entry as? DateTimeFloatEntry }
+    }
+
+    private fun drawGuideline(
+        context: DrawContext,
+        bounds: RectF,
+        markedEntries: List<Marker.EntryModel>,
+    ) {
+        markedEntries
+            .map { it.location.x }
+            .toSet()
+            .forEach { x ->
+                guideline?.drawVertical(
+                    context,
+                    bounds.top,
+                    bounds.bottom,
+                    x,
+                )
+            }
     }
 }
