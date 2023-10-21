3e15b3362f9a64de7d6c02d34758d38b104e41fd
Al Priest <alpriest@gmail.com>
2023-10-21 07:59:15 +0100

    Add toggle formula view

--- app/src/main/java/com/alpriest/energystats/ui/statsgraph/ApproximationView.kt
+++ app/src/main/java/com/alpriest/energystats/ui/statsgraph/ApproximationView.kt
@@ -3,6 +3,7 @@ package com.alpriest.energystats.ui.statsgraph
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,14 +11,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -55,6 +67,7 @@ fun ApproximationView(themeStream: MutableStateFlow<AppTheme>, modifier: Modifie
         SelfSufficiencyEstimateMode.Net -> viewModel.netSelfSufficiencyEstimate
         SelfSufficiencyEstimateMode.Absolute -> viewModel.absoluteSelfSufficiencyEstimate
     }
+    var showingApproximations by remember { mutableStateOf(false) }
 
     Box(modifier) {
         Column(
@@ -181,6 +194,22 @@ fun ApproximationView(themeStream: MutableStateFlow<AppTheme>, modifier: Modifie
                 .padding(horizontal = 2.dp, vertical = 1.dp),
             color = ApproximationHeaderText,
         )
+
+        Icon(
+            imageVector = if (showingApproximations) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+            contentDescription = null,
+            tint = ApproximationHeaderText,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .offset(x = (-8).dp, y = (-11).dp)
+                .height(21.dp)
+                .background(
+                    ApproximationHeader,
+                    shape = RoundedCornerShape(size = 4.dp)
+                )
+                .padding(horizontal = 2.dp, vertical = 1.dp)
+                .clickable { showingApproximations = !showingApproximations }
+        )
     }
 }
 
@@ -203,7 +232,8 @@ fun StatsApproximationViewPreview() {
                     batteryCharge = 2.3,
                     batteryDischarge = 1.2
                 )
-            )
+            ),
+            modifier = Modifier.padding(24.dp)
         )
     }
 }
