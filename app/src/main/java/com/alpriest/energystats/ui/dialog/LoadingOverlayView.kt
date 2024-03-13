a45e8ee43dc2420d29a72b1500fc1519b7b6e5f9
Al Priest <alpriest@gmail.com>
2024-03-13 09:26:20 +0000

    Tweaked loading view

--- app/src/main/java/com/alpriest/energystats/ui/dialog/LoadingOverlayView.kt
+++ app/src/main/java/com/alpriest/energystats/ui/dialog/LoadingOverlayView.kt
@@ -1,12 +1,14 @@
 package com.alpriest.energystats.ui.dialog
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,12 +18,14 @@ import com.alpriest.energystats.ui.theme.EnergyStatsTheme
 
 @Composable
 fun LoadingOverlayView() {
-    Column(
-        Modifier.background(colors.secondary).padding(4.dp)
+    Card(
+        border = BorderStroke(1.dp, Color.Gray),
+        shape = RectangleShape
     ) {
         Text(
             stringResource(R.string.loading),
-            color = colors.onSecondary
+            color = colors.onSecondary,
+            modifier = Modifier.padding(8.dp)
         )
     }
 }
