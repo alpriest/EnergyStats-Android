8c329e96e8757cc42def944fc36f14c6770e6e5c
Al Priest <alpriest@gmail.com>
2024-10-22 07:44:06 +0100

    Update to Kotlin syntax build and gradle wrapper files

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+val localProperties = Properties()
+localProperties.load(FileInputStream(rootProject.file("local.properties")))
+
+android {
+    namespace = "com.alpriest.energystats"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.alpriest.energystats"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 216
+        versionName = "2.70"
+
+        buildConfigField(type = "String", name = "GOOGLE_MAPS_APIKEY", value = localProperties.getProperty("GOOGLE_MAPS_APIKEY"))
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material.icons)
+    implementation(libs.composeMaterial3)
+    implementation(libs.composeActivity)
+    implementation(libs.glance.appwidget)
+    implementation(libs.glance.material3)
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.systemuicontroller)
+    implementation(libs.gson)
+    implementation(libs.okhttp)
+    implementation(libs.composePreview)
+    implementation(libs.navigationKtx)
+    implementation(libs.navigationCompose)
+    implementation(libs.coilCompose)
+    implementation(libs.compose.material.dialogs)
+    implementation(libs.composeMarkdown)
+    implementation(libs.shimmer)
+    implementation(libs.vico)
+    implementation(libs.lifecycle.process)
+    implementation(libs.lifecycle.runtimeKtx)
+    implementation(libs.lifecycle.viewmodelCompose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+}
