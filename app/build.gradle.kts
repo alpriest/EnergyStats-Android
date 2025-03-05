e8a698c9f0a4a7b08368e0c5daa4ac9f6a2bc2e6
Al Priest <alpriest@gmail.com>
2025-03-05 12:51:01 +0000

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.alpriest.energystats"
         minSdk = 26
         targetSdk = 34
-        versionCode = 253
-        versionName = "2.98"
+        versionCode = 254
+        versionName = "2.99"
 
         buildConfigField(type = "String", name = "GOOGLE_MAPS_APIKEY", value = localProperties.getProperty("GOOGLE_MAPS_APIKEY"))
     }
