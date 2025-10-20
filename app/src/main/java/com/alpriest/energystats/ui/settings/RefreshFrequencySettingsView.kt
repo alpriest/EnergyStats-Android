8220d016947c8be1b34fb1ba3b8cb8881ad891c7
Nelson Scheja <n.scheja@gmx.de>
2025-10-21 01:49:25 +0200

    Correct/improve german translation and some strings in general

--- app/src/main/java/com/alpriest/energystats/ui/settings/RefreshFrequencySettingsView.kt
+++ app/src/main/java/com/alpriest/energystats/ui/settings/RefreshFrequencySettingsView.kt
@@ -29,7 +29,7 @@ fun RefreshFrequencySettingsView(config: ConfigManaging) {
 
     SettingsColumnWithChild {
         SettingsSegmentedControl(
-            title = "Refresh frequency",
+            title = stringResource(R.string.refresh_frequency),
             segmentedControl = {
                 val items = RefreshFrequency.values()
 
