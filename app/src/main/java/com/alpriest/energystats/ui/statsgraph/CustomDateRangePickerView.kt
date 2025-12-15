d5d9a7b23a9714645bb2a402d7124ad992194446
Al Priest <alpriest@gmail.com>
2025-12-15 18:42:54 +0000

    Start to draw out the date range picker view

--- app/src/main/java/com/alpriest/energystats/ui/statsgraph/CustomDateRangePickerView.kt
+++ app/src/main/java/com/alpriest/energystats/ui/statsgraph/CustomDateRangePickerView.kt
@@ -1,10 +1,84 @@
 package com.alpriest.energystats.ui.statsgraph
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.alpriest.energystats.ui.helpers.SegmentedControl
+import com.alpriest.energystats.ui.settings.SettingsColumn
+import com.alpriest.energystats.ui.settings.SettingsPage
+import com.alpriest.energystats.ui.settings.SettingsSegmentedControl
+import com.alpriest.energystats.ui.theme.ESButton
+import java.time.LocalDate
+
+enum class CustomDateRangeDisplayUnit {
+    DAYS,
+    MONTHS;
+
+    val title: String
+        get() = when (this) {
+            DAYS -> "Days"
+            MONTHS -> "Months"
+        }
+}
 
 @Composable
 fun CustomDateRangePickerView() {
-    Text("Custom here")
+    var start = remember { LocalDate.now() }
+    var end = remember { LocalDate.now() }
+    val context = LocalContext.current
+    var viewBy = remember { CustomDateRangeDisplayUnit.MONTHS }
+
+    SettingsPage {
+        SettingsColumn(header = "Quick choice") {
+            Row {
+                ESButton(
+                    onClick = {
+                        start = LocalDate.now().minusMonths(12)
+                        end = LocalDate.now()
+                    },
+                    content = {
+                        Text("Last 12 months")
+                    }
+                )
+            }
+
+            Row {
+                ESButton(
+                    onClick = {
+                        start = LocalDate.now().minusMonths(6)
+                        end = LocalDate.now()
+                    },
+                    content = {
+                        Text("Last 6 months")
+                    }
+                )
+            }
+        }
+
+        SettingsColumn(header = "View by") {
+            SettingsSegmentedControl(
+                segmentedControl = {
+                    val items = CustomDateRangeDisplayUnit.entries
+                    SegmentedControl(
+                        items = items.map { it.title },
+                        defaultSelectedItemIndex = items.indexOf(viewBy),
+                        color = colorScheme.primary
+                    ) {
+                        viewBy = items[it]
+                    }
+                }
+            )
+        }
 //    viewModel.rangeStream.value = DatePickerRange.CUSTOM(LocalDate.now().minusDays(30), LocalDate.now())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CustomDateRangePickerViewPreview() {
+    CustomDateRangePickerView()
 }
