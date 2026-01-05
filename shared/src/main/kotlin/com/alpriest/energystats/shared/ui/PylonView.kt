2d381a08165d0159e9d28bd73891c9702f9ce472
Al Priest <alpriest@gmail.com>
2026-01-05 19:00:22 +0000

    Better small PylonView

--- shared/src/main/kotlin/com/alpriest/energystats/shared/ui/PylonView.kt
+++ shared/src/main/kotlin/com/alpriest/energystats/shared/ui/PylonView.kt
@@ -1,12 +1,15 @@
 package com.alpriest.energystats.shared.ui
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,6 +24,7 @@ fun PylonView(modifier: Modifier, color: Color, strokeWidth: Float) {
         val leftLegTop = Offset(x = hSize * 4, y = 0f - (strokeWidth / 2.0f))
         val rightLegBottom = Offset(x = hSize * 7.5f, y = size.height)
         val rightLegTop = Offset(x = hSize * 6.0f, y = 0f - (strokeWidth / 2.0f))
+        val topBarInset = size.width / 100f
 
         drawLine(
             color = color,
@@ -30,8 +34,8 @@ fun PylonView(modifier: Modifier, color: Color, strokeWidth: Float) {
         )
         drawLine(
             color = color,
-            start = leftLegTop.minus(Offset(3f, -1f)),
-            end = rightLegTop.plus(Offset(3f, 1f)),
+            start = leftLegTop.minus(Offset(topBarInset, -1f)),
+            end = rightLegTop.plus(Offset(topBarInset, 1f)),
             strokeWidth = strokeWidth
         )
         drawLine(
@@ -67,4 +71,28 @@ fun PylonView(modifier: Modifier, color: Color, strokeWidth: Float) {
             strokeWidth = strokeWidth
         )
     }
+}
+
+@Preview
+@Composable
+fun PylonViewPreviewWatch() {
+    PylonView(
+        modifier = Modifier
+            .height(30.dp)
+            .width(30.dp * 1.3f),
+        Color.Yellow,
+        4f
+    )
+}
+
+@Preview
+@Composable
+fun PylonViewPreviewPhone() {
+    PylonView(
+        modifier = Modifier
+            .height(60.dp)
+            .width(60.dp * 1.3f),
+        Color.Yellow,
+        6f
+    )
 }
--- shared/src/main/kotlin/com/alpriest/energystats/shared/ui/SunIcon.kt
+++ shared/src/main/kotlin/com/alpriest/energystats/shared/ui/SunIcon.kt
@@ -153,7 +153,6 @@ fun SunIcon(
     }
 }
 
-
 @Preview
 @Composable
 fun SunIconPreviewWatch() {
