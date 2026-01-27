4a1fa55fdba8dab480920236efed19c1c3df00d1
Al Priest <alpriest@gmail.com>
2026-01-27 18:52:10 +0000

    Enable edgeToEdge for old devices

--- app/src/main/java/com/alpriest/energystats/MainActivity.kt
+++ app/src/main/java/com/alpriest/energystats/MainActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         val appContainer = (application as EnergyStatsApplication).appContainer
@@ -23,11 +24,9 @@ class MainActivity : ComponentActivity() {
             appContainer.solarForecastingProvider,
             appContainer.credentialStore
         )
-        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             PreHomeView(appContainer = appContainer, viewModel = preHomeViewModel)
         }
     }
 }
-
