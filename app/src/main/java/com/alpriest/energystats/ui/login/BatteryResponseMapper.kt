3d497301a3d4636b5c72d650bfe075922bfde43e
Al Priest <alpriest@gmail.com>
2026-04-01 16:17:53 +0100

    Read the scaled battery size correctly

--- app/src/main/java/com/alpriest/energystats/ui/login/BatteryResponseMapper.kt
+++ app/src/main/java/com/alpriest/energystats/ui/login/BatteryResponseMapper.kt
@@ -4,20 +4,33 @@ import com.alpriest.energystats.shared.models.Battery
 import com.alpriest.energystats.shared.models.network.BatterySOCResponse
 import com.alpriest.energystats.shared.models.network.OpenRealQueryResponse
 import com.alpriest.energystats.shared.models.network.SoC
-import com.alpriest.energystats.shared.models.network.currentValue
+import com.alpriest.energystats.shared.models.network.currentData
 
 enum class BatteryResponseMapper {
     ;
+
     companion object {
         fun map(batteryVariables: OpenRealQueryResponse, settings: BatterySOCResponse): Battery {
-            val residual = batteryVariables.datas.currentValue("ResidualEnergy")
+            val residualData = batteryVariables.datas.currentData("ResidualEnergy")
+            val residual = residualData?.value ?: 0.0
+
+            var unitWh = 10.0
+            residualData?.unit?.trim()?.let {
+                unitWh = when {
+                    it.endsWith("kWh") -> it.removeSuffix("kWh").trim().toDouble() * 1000.0
+                    it.endsWith("Wh") -> it.removeSuffix("Wh").trim().toDouble()
+                    else -> unitWh
+                }
+            }
+
             val soc = batteryVariables.datas.SoC()
 
             val batteryCapacity: String = if (soc > 0) {
-                ((residual * 10.0) / (soc / 100.0)).toInt().toString()
+                ((residual * unitWh) / (soc / 100.0)).toInt().toString()
             } else {
                 "0"
             }
+
             val minSOC: String = (settings.minSocOnGrid.toDouble() / 100.0).toString()
 
             return Battery(capacity = batteryCapacity, minSOC = minSOC, hasError = false)
